05fb57e03c5273080ddcf75605f5a71752e99070
Sayed Mubarish <mubarish007@gmail.com>
2025-11-03 12:01:51 +0530

    feat:create a embedding pipeline and embed topics

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from src import data_extractor
 from src import document_structure
+from src import embedding
 
 if __name__ == "__main__":
     url = "https://docs.langchain.com/llms.txt"
-    topics,urls = data_extractor.extract_topics_urls(url)
-   
+    topics, urls = data_extractor.extract_topics_urls(url)
+
+    document_structures = document_structure.create_document_structures(topics, urls)
+
+    emb_pipe = embedding.EmbeddingPipeline()
+
+    topic_embeddings = emb_pipe.embed_topic(topics)
     
-    document_structures = document_structure.create_document_structures(topics,urls)
-    
-    print(document_structures[1].__dict__)
---
+++ src/embedding.py
@@ -0,0 +1,46 @@
+from typing import Any,List
+from sentence_transformers import SentenceTransformer
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import numpy as np
+
+class EmbeddingPipeline:
+    def __init__(
+        self,
+        model_name: str = "all-MiniLM-L6-v2",
+        chunk_size: int = 1000,
+        chunk_overlap: int = 200,
+    ):
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+        self.model = SentenceTransformer(model_name)
+
+        print(f"[INFO] Loaded embedding model: {model_name}")
+
+    def embed_topic(self,topics: List[Any]) -> np.ndarray:
+        print(f"[INFO] Generate embeddings for topics.. {len(topics)} topics")
+        topic_embeddings = self.model.encode(topics,show_progress_bar=True)
+        return topic_embeddings
+
+    def chunk_documents(self, documents: List[Any]) -> List[Any]:
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=self.chunk_size,
+            chunk_overlap=self.chunk_overlap,
+            length_function=len,
+            separators=["\n\n", "\n", " ", ""],
+        )
+        chunks = text_splitter.split_documents(documents)
+        print(f"[INFO] Split {len(documents)} documents into {len(chunks)} chunks.")
+        return chunks
+
+    def embed_chunks(self, chunks: List[Any]) -> np.ndarray:
+        texts = [chunk.page_content for chunk in chunks]
+        print(f"[INFO] Generate embeddings for {len(chunks)} chunks....")
+        embeddings = self.model.encode(texts,show_progress_bar=True)
+        print(f"Embeddings shape:{embeddings.shape}")
+        return embeddings
+
+    def embed_query(self,query:str) -> np.ndarray:
+        print(f"[INFO]Generate embedding for query")
+        query_embedding = self.model.encode(query)
+        print(f"Embeddings shape:{query_embedding.shape}")
+        return query_embedding
