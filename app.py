c4cadc5ad39a96294022a74addfa0ffa397c4d45
Sayed Mubarish <mubarish007@gmail.com>
2025-11-03 11:37:04 +0530

    feat:create a module for creating document structures

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from src import data_extractor
+from src import document_structure
 
 if __name__ == "__main__":
     url = "https://docs.langchain.com/llms.txt"
     topics,urls = data_extractor.extract_topics_urls(url)
-    print(f"TOPICS: {topics}")
-    print(f"URLS: {urls}")
+   
+    
+    document_structures = document_structure.create_document_structures(topics,urls)
+    
+    print(document_structures[1].__dict__)
---
+++ src/document_structure.py
@@ -0,0 +1,21 @@
+import uuid
+from langchain_core.documents import Document
+from typing import List, Any
+
+
+def create_document_structures(topics: List[Any], urls: List[Any]) -> List[Any]:
+
+    document_structures = []
+
+    for i, (topic, url) in enumerate(zip(topics, urls)):
+        doc = Document(
+            id=f"doc_{uuid.uuid4().hex[:8]}_{i}",
+            page_content=topic,
+            metadata={"url": url},
+        )
+        document_structures.append(doc)
+
+    print(
+        f"[INFO] Created document stuctures containing {len(document_structures)} documents"
+    )
+    return document_structures
